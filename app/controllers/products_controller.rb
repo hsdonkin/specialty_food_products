f478ed9f57e15db5e211efe646a23c7ed5ea84ea
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-09 13:55:49 -0700

    fix some css issues, and implement review cards on product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,13 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def show
+    id = params[:id]
+    @product = Product.find(id)
+    country_id = @product.country_id
+    @country = Country.find(country_id)
+    @reviews = Review.where("product_id =#{id}")
+    render :product
+  end
 end
