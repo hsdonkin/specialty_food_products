4e42eab549ad1c8563377520148375d37bd26b07
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-09 09:38:35 -0700

    create add product form

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,12 @@
 class AdminsController < ApplicationController
 
   def show
+    @countries = CountryOfOrigin.all
     render :edit_products
+  end
+
+  def create
+    name = params[:name]
+    price = params[:price]
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   has_many :reviews
   belongs_to :country_of_origin
   before_save {self.name.titleize!}
+
+  validates :name, presence: true
+  validates :country_id, presence: true
+  validates :price, presence: true
 end
