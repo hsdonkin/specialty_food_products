b35cf78a12d50e906271553e4ce40d08dc2a77aa
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-09 11:13:07 -0700

    use flash message on edit page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,7 +6,20 @@ class AdminsController < ApplicationController
   end
 
   def create
+    # this has to be here for the refresh
+    @countries = Country.all
+
     name = params[:name]
     price = params[:price]
+    country_id = params["Country of Origin"].to_i
+    # if the product is made successfully
+    if Product.create!(:name => name, :cost => price, :country_id => country_id)
+      flash[:notice] = {:message => "#{name} added successfully!", :class => "alert alert-success"}
+      redirect_to :controller => 'admins', :action => 'show'
+    else
+      flash[:bad] = {:message => "Whoops! Try again!", :class => "alert alert-danger"}
+      redirect_to :controller => 'admins', :action => 'show'
+    end
+
   end
 end
