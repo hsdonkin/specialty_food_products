4cb68732670ae760ee3fe1f5a22bf96fef902afb
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-16 14:44:13 -0700

    users can add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
+
+  def create
+
+    @product = Product.find(params[:product_id])
+    content_body = params[:content_body]
+    rating = params[:rating]
+    current_user.reviews.create!(:content_body => content_body, :rating => rating, :product_id => @product.id)
+    redirect_to product_path(@product)
+  end
+
 end
