ef4dc0507df0dd662edbeb0e85e1fa0aa69cf850
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-16 15:04:13 -0700

    adds flash messages to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,15 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     content_body = params[:content_body]
     rating = params[:rating]
-    current_user.reviews.create!(:content_body => content_body, :rating => rating, :product_id => @product.id)
-    redirect_to product_path(@product)
+    review = current_user.reviews.new(:content_body => content_body, :rating => rating, :product_id => @product.id)
+    if review.save
+      flash[:notice] = {:message=>"Thanks! Your review for #{@product.name} has been added!", :class=>"alert alert-success"}
+      redirect_to product_path(@product)
+    else
+      flash[:notice] = {:message=>review.errors.full_messages, :class=>"alert alert-danger"}
+      redirect_to product_path(@product)
+    end
+
   end
 
 end
