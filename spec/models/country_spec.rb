0d3b97cec62bb52c7034286bca7f324fec44b8db
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-19 20:47:42 -0700

    validate uniqueness of country names, test pass

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 require 'factory_bot'
 
+FactoryBot.find_definitions
 RSpec.describe Country do
   it "will have a name" do
-    FactoryBot.find_definitions
     country = FactoryBot.create(:country)
     expect(country.name).to match (/[a-z]+/)
+  end
+
+  it "will be a unique list of 10 countries" do
+    duplicate = false
+    names = []
+    countries = FactoryBot.create_list(:country, 10)
+    countries.each do |c|
+      names.push(c.name)
+      if names.count(c.name) > 1
+        binding.pry
+        duplicate = true
+      end
+    end
+    expect(duplicate).to eq (false)
   end
 end
